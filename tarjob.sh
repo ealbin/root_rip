fa8aa5b1964f508998b355b9189dd5334522b283
Eric Albin <Eric.K.Albin@gmail.com>
2017-10-25 21:24:03 +0000

    updated tarfile.py

--- tarjob.sh
+++ tarjob.sh
@@ -3,8 +3,7 @@
 # bundles tar.gz data files specified in spiderjobs as a tar.gz
 # transmitts back to gpatlas
 
-echo '[craydata]: preparing files'
-echo '----------------------------'
+echo '   [craydata]: preparing files for transfer...'
 
 if [ $# -eq 0 ]; then
     echo 'sausage: bash tarjob.sh [job_####.conf]'
@@ -15,7 +14,8 @@ home=/home/ealbin/root_rip/spiderjobs
 out=${1/%conf}tar.gz  # e.g. job_0123.conf -> job_0123.tar.gz
 cd /home/ealbin/root_rip/donejobs/${1/%.conf}
 tar -czvPf $home/$out -T $home/$1 ./*.root
+echo '   [craydata]: transfering...'
 scp $home/$out ealbin@gpatlas1.ps.uci.edu:$home
 rm $home/$out
 
-echo '[craydata]: done!'
+
