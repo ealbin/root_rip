1ecd531dc5a13d30eea050ad4adc20b96a658281
Eric Albin <Eric.K.Albin@gmail.com>
2017-10-29 21:35:08 +0000

    yup

--- tarjob.sh
+++ tarjob.sh
@@ -10,12 +10,15 @@ if [ $# -eq 0 ]; then
     exit
 fi
 
+if [ -e /home/ealbin/root_rip/donejobs/${1/%.conf} ]; then
+    rm -rf /home/ealbin/root_rip/donejobs/${1/%.conf} ]
+fi
+mkdir -p /home/ealbin/root_rip/donejobs/${1/%.conf}
+
 home=/home/ealbin/root_rip/spiderjobs
 out=${1/%conf}tar.gz  # e.g. job_0123.conf -> job_0123.tar.gz
 cd /home/ealbin/root_rip/donejobs/${1/%.conf}
-tar -czvPf $home/$out -T $home/$1 ./*.root
+tar -czvPf $home/$out -T $home/$1
 echo '   [craydata]: transfering...'
 scp $home/$out ealbin@gpatlas1.ps.uci.edu:$home
 rm $home/$out
-
-
